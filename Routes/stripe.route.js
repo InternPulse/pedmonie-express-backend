3383e3a17788f89b73479c870ed95bbb762749fd
Maxim-LD <ayinde779@gmail.com>
2025-02-16 12:36:27 +0100

    route address changed

--- Routes/stripe.route.js
+++ Routes/stripe.route.js
@@ -3,7 +3,7 @@ const { orderCheckout, verifyCheckout } = require('../services/stripe/index')
 
 const router = express.Router()
 
-router.post("/stripe_orders", orderCheckout)
-router.get("/stripe_orders/:session_id", verifyCheckout)
+router.post("/orders-stripe", orderCheckout)
+router.get("/orders-stripe/:session_id", verifyCheckout)
 
 module.exports = router
