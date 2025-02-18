026b8f0b1154e690d3d9683e73fbbcff1e5287b5
Maxim-LD <ayinde779@gmail.com>
2025-02-18 12:46:49 +0100

    updated stripe routes

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.listen(port, ()=>{
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json())
-app.use(PaypalRouter)
-app.use(stripeRouter)
+app.use("/api/v1", PaypalRouter)
+app.use("/api/v1", stripeRouter)
