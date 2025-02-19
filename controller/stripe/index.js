c8375e278ebae2a22e54d6695d169d0201ca7591
Maxim-LD <ayinde779@gmail.com>
2025-02-19 22:39:57 +0100

    refactor transaction handling; enhance order retrieval logic and add transaction existence check

--- controller/stripe/index.js
+++ controller/stripe/index.js
@@ -6,12 +6,18 @@ const Wallet = require("../../models/wallet.js")(sequelize, require("sequelize")
 const Merchant = require("../../models/merchant.js")(sequelize, require("sequelize").DataTypes)
 
 // Helper function to handle transactions and order retrieval
-const getOrderForTransaction = async (order_id, t) => {
-  const order = await Order.findOne({ where: { order_id } }, { transaction: t })
+const getOrderForTransaction = async (order_id, merchant_id, t) => {
+  let order
+  if (order_id) {
+    order = await Order.findOne({ where: { order_id }, transaction: t })
+  } else if (merchant_id) {
+    order = await Order.findOne({ where: { merchant_id }, transaction: t })
+  }
+
   if (!order) {
     return { status: 404, message: "Order not found" }
   }
-    return order
+  return order
 }
 
 const createUnpaidOrder = async (orderData) => {
@@ -34,55 +40,62 @@ const createUnpaidOrder = async (orderData) => {
     return { status: 500, message: "Error creating order status: " + error.message  }
   }    
 }
-
-// Update order in the database
-const updateOrderStatus = async (order_id, payment_status, { transaction: t }) => {
-  try {
-      const order = await Order.update(
-        { payment_status }, // Update the payment_status
-        { where: { order_id }, transaction: t } // Find the order by order_id
-      )
-    return { status: 200, message: "Order status updated successfully", order }
-  } catch (error) {
-      return { status: 500, message: "Error updating order status: " + error.message }
-  }
-}
-
 const createTransaction = async (transactionData, { transaction: t }) => {
   try {
-     const order = await getOrderForTransaction(transactionData.order_id, t)
-      const transaction = await Transaction.create(
-        {
-          order_id: order.order_id,
-          merchant_id: order.merchant_id,
-          gateway_name: "Stripe",
-          gateway_transaction_identifier: transactionData.gateway_transaction_identifier,
-          amount: order.amount,
-          payment_channel: transactionData.payment_method_types[0],
-          status: "successful",
-          currency: order.currency,
-        },
-        { transaction: t }
-      )
-    return { status: 201, message: "Transaction created successfully", transaction }
+    // Check if the transaction already exists and is verified
+    const existingTransaction = await Transaction.findOne({
+      where: {
+        order_id: transactionData.order_id,
+        merchant_id: transactionData.merchant_id,
+        status: "successful",
+      },
+      transaction: t,
+    })
+    if (existingTransaction) {
+      return {
+        status: 409,
+        message: "Transaction already exists and is verified",
+        transaction: existingTransaction,
+      }
+    }
+    const transaction = await Transaction.create(
+      {
+        order_id: transactionData.order_id,
+        merchant_id: transactionData.merchant_id,
+        gateway_name: "Stripe",
+        gateway_transaction_identifier: transactionData.gateway_transaction_identifier,
+        amount: transactionData.amount,
+        payment_channel: "card",
+        status: "successful",
+        currency: transactionData.currency,
+      },
+      { transaction: t }
+    )
+    return {
+      status: 201,
+      message: "Transaction created successfully",
+      transaction,
+    }
   } catch (error) {
     return { status: 500, message: "Error creating transaction: " + error.message }
   }
  
 }
-
 const creditWallet = async (walletData, { transaction: t }) => {
   try {
-    const order = await getOrderForTransaction( walletData.order_id, t)
+    const order = await getOrderForTransaction(null, walletData.merchant_id, t)
+    if (order.status === 404) {
+      return order
+    }
     // Check if wallet exists
-    const wallet = await Wallet.findOne({
+    let wallet = await Wallet.findOne({
       where: { merchant_id: order.merchant_id },
       transaction: t,
     })
     if (wallet) {
       // If wallet exists, update balance
       await wallet.update(
-        { amount: wallet.amount + order.amount },
+        { amount: parseFloat(wallet.amount) + parseFloat(order.amount) },
         { transaction: t }
       )
     } else {
@@ -106,7 +119,6 @@ const creditWallet = async (walletData, { transaction: t }) => {
 
 module.exports = {
   createUnpaidOrder,
-  updateOrderStatus,
   createTransaction,
   creditWallet
 }
