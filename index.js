2f14f0bbc42d9e27e2bbc8fa6576f3bb13a8beb6
Abass Ibrahim <profbass4all@gmail.com>
2025-02-23 07:48:37 +0100

    update

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ const limiter = require("./middlewares/rateLimiter");
 
 
 
-const PaystackRouter = require("./Routes/paystack.route");
-const PaymentRouter = require("./Routes/paymentprocessing.route");
+
 const stripeRouter = require("./Routes/stripe.route");
-const flutterwaveRouter = require("./Routes/flutterwave.route");
 const monnifyRouter = require("./Routes/monnify.route");
 
 const port = process.env.APP_PORT || 1111;
@@ -39,12 +37,6 @@ async function connection(){
     }
 } 
 connection()
-
-
-
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({ limit: "5mb" }));
 app.use(cors());
@@ -67,25 +59,3 @@ app.use("/api/v1/payments", monnifyRouter);
 app.get("/", (req, res) => {
 	res.send("Welcome to Pedmonie Express API");
 });
-
-const startServer = async () => {
-	try {
-		if (sequelize) {
-			await sequelize.authenticate();
-			console.info(
-				"Database connection has been established successfully."
-			);
-			// Optionally synchronize the database:
-			 //await sequelize.sync({ alter: true });
-			 //console.info("Database synchronized.");
-		}
-		app.listen(port, () =>
-			console.info(`🚀 Server running on port ${port}`)
-		);
-	} catch (error) {
-		console.error("Error starting server:", error.message);
-		process.exit(1);
-	}
-};
-
-startServer();
