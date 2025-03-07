9f1391ab08d3fdf0f5b6fd419256ff762c346abb
Maxim-LD <ayinde779@gmail.com>
2025-03-07 10:47:10 +0100

    add stripe routers; update connection logic and middleware usage

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 const express  = require('express')
 require('dotenv').config()
 const bodyParser = require('body-parser')
-const PaypalRouter = require('./Routes/paypal.route')
 
-// const stripeRouter = require('./Routes/stripe.route')
+const PaypalRouter = require('./Routes/paypal.route')
 const PaystackRouter = require('./Routes/paystack.route')
 const PaymentRouter = require('./Routes/paymentprocessing.route')
 const flutterwaveRouter = require('./Routes/flutterwave.route');
+const stripeRouter = require("./Routes/stripe.route")
+const monnifyRouter = require("./Routes/monnify.route")
 // const Merchant = require('./models/merchants.js')
 
 
@@ -15,17 +16,17 @@ const cors = require("cors");
 const helmet = require("helmet");
 const limiter = require("./middlewares/rateLimiter");
 
-// const stripeRouter = require("./Routes/stripe.route");
-const monnifyRouter = require("./Routes/monnify.route");
+
 
 const port = process.env.APP_PORT || 1111;
 const app = express();
 const { sequelize } = require("./models/index");
+const { connectToDb } = require('./Testin/schema')
 
 async function connection(){
     try{
-        await sequelize.sync()
-        console.log('Connection has been established successfully.');
+        //await sequelize.sync()
+        //console.log('Connection has been established successfully.');
         app.listen(port, ()=>{
         console.log(`Server running on port ${port}`)
 })
@@ -34,23 +35,21 @@ async function connection(){
     }
 } 
 connection()
+
+//stripe testing
+connectToDb()
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({ limit: "5mb" }));
 app.use(cors());
 app.use(helmet());
 app.use(limiter);
+
 app.use("/api/v1", PaymentRouter);
-
 app.use("/api/v1", PaypalRouter);
-
-
 app.use("/api/v1", PaystackRouter)
-
-// app.use('/api/v1', stripeRouter)
-
-
+app.use('/api/v1', stripeRouter)
 app.use("/api/v1", flutterwaveRouter);
-
 app.use("/api/v1/payments", monnifyRouter);
 
 app.get("/", (req, res) => {
